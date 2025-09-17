4c44558f5a45f6354db2020883950e8da0abbb89
untwist <40829152+untwist@users.noreply.github.com>
2025-09-17 16:47:13 -0400

    Fix: Update base path for GitHub Pages deployment

--- ml-simulations/vite.config.ts
+++ ml-simulations/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ml-simulations/', // This will be your GitHub repository name
+  base: '/AI4C/', // This will be your GitHub repository name
   build: {
     outDir: 'dist',
     sourcemap: true
